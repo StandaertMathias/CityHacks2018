213103d93919fad4b663396504413287a8fab56c
Mathias Standaert <mathias.standaert@student.howest.be>
2018-11-09 16:21:23 +0100

    log route

--- public/script.js
+++ public/script.js
@@ -1,7 +1,36 @@
-$(document).ready(function(){
+let route = [];
+let stop = false;
+let id = 0;
+let map;
+function startRunning() {
+    id = window.setInterval(function () {
+        if (!stop) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                route.push(
+                    {
+                        'x': position.coords.latitude,
+                        'y': position.coords.longitude
+                    });
+
+            });
+        } else {
+            window.clearInterval(id);
+            for(var i = 0; i<route.length; i++){
+                addMarker(map, route[i].x, route[i].y, 'route');
+            }
+        }
+    }, 1000);
+}
+
+$(document).ready(function () {
     // add code here
     getLocation();
+    startRunning();
+    $('#stop').on('click', function () {
+        stop = true;
+    })
 });
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
@@ -9,8 +38,9 @@ function getLocation() {
         console.log("Geolocation is not supported by this browser.");
     }
 }
+
 function showPosition(position) {
-    var map = L.map('map').setView([position.coords.latitude,position.coords.longitude], 15);
+    map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 15);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
@@ -18,6 +48,6 @@ function showPosition(position) {
 }
 
 
-function addMarker(map, x,y, type){
-        L.marker([x, y]).addTo(map);
+function addMarker(map, x, y, type) {
+    L.marker([x, y]).addTo(map);
 }
